107ec13e43af0a23125d4a693db830b54b4ef3bb
Jerry-zhk <jchong0618@gmail.com>
2018-11-14 00:19:13 +0800

    establish connection

--- crypto.js
+++ crypto.js
@@ -2,8 +2,10 @@ const crypto = require('crypto');
 const fs = require('fs');
 const assert = require('assert');
 
+const passphrase = 'AKSenejfkwnEjDniw';
+
 // generate RSA keys, require node >= v10.12.0
-function generateKeyPair(){
+function generateKeyPair() {
   const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
     publicKeyEncoding: {
@@ -14,55 +16,46 @@ function generateKeyPair(){
       type: 'pkcs1',
       format: 'pem',
       cipher: 'aes-256-cbc',
-      passphrase: 'AKSenejfkwnEjDniw'
+      passphrase: passphrase
     }
   });
-  fs.writeFile('publicKey.pem', publicKey, () => {console.log('Wrote publicKey.pem.')})
-  fs.writeFile('privateKey.pem', privateKey, () => {console.log('Wrote privateKey.pem.')})
-  console.log(publicKey)
-  console.log(privateKey)
+  // fs.writeFile('publicKey.pem', publicKey, () => {console.log('Wrote publicKey.pem.')})
+  // fs.writeFile('privateKey.pem', privateKey, () => {console.log('Wrote privateKey.pem.')})
+  console.log(publicKey);
+  console.log(privateKey);
 }
 
 // generateKeyPair();
 
-// const algorithm = 'aes-256-cbc';
-// let key = Buffer.alloc(32); // key should be 32 bytes
-// let iv = Buffer.alloc(16); // iv should be 16
+const algorithm = 'aes-256-cbc';
 
-// key = Buffer.concat([Buffer.from('password')], key.length);
-// iv = Buffer.from(Array.prototype.map.call(iv, () => {return Math.floor(Math.random() * 256)}));
+// return { ciphertext, iv }
+const encryptWithAES256CBC = (key, plaintext) => {
+  let iv = Buffer.alloc(16);
+  iv = Buffer.from(Array.prototype.map.call(iv, () => { return Math.floor(Math.random() * 256) }));
+  let cipher = crypto.createCipheriv(algorithm, key, iv);
+  let ciphertext = cipher.update(plaintext, 'utf8', 'hex');
+  ciphertext += cipher.final('hex');
+  return { ciphertext, iv: iv.toString('hex') };
+}
 
-// console.log('key: ', key.toString('hex'));
-// console.log('iv: ', iv.toString('hex'));
-
-// let cipher = crypto.createCipheriv(algorithm, key, iv);
-// let encrypted = cipher.update('some clear text data', 'utf8', 'hex');
-// encrypted += cipher.final('hex');
-// console.log('ciphertext: ',encrypted);
-
-// let decipher = crypto.createDecipheriv(algorithm, key, iv);
-// let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-// decrypted += decipher.final('utf8');
-// console.log('plaintext: ',decrypted);
+const decryptWithAES256CBC = (key, hexIV, ciphertext) => {
+  let iv = Buffer.from(hexIV, 'hex')
+  let decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let plaintext = decipher.update(ciphertext, 'hex', 'utf8');
+  plaintext += decipher.final('utf8');
+  return plaintext;
+}
 
 
 // Diffie Hellman
-// const alice = crypto.createDiffieHellman(1024);
-// const aliceKey = alice.generateKeys();
-// console.log(aliceKey.toString('hex'));
+const generateSessionKeys = (clientKey) => {
+  const dh = crypto.getDiffieHellman('modp5');
+  const serverKey = dh.generateKeys();
+  const sessionKey = dh.computeSecret(clientKey);
+  return { serverKey, sessionKey };
+}
 
-// // Generate Bob's keys...
-// const bob = crypto.createDiffieHellman(alice.getPrime(), alice.getGenerator());
-// const bobKey = bob.generateKeys();
-
-// // Exchange and generate the secret...
-// const aliceSecret = alice.computeSecret(bobKey);
-// const bobSecret = bob.computeSecret(aliceKey);
-// console.log(aliceSecret)
-// console.log(bobSecret)
-
-// // OK
-// assert.strictEqual(aliceSecret.toString('hex'), bobSecret.toString('hex'));
 
 // const hmacAlgorithm = 'sha256';
 // const hmac = crypto.createHmac(hmacAlgorithm, 'a secret');
@@ -71,41 +64,39 @@ function generateKeyPair(){
 // let digest = hmac.digest('hex');
 // console.log(digest);
 
-function getPublicKey(){
+function getPublicKey() {
   return fs.readFileSync('publicKey.pem', 'utf8');
 }
 
-function getPrivateKey(){
-  return fs.readFileSync('privateKey.pem', 'utf8');
+const privateKey = fs.readFileSync('privateKey.pem', 'utf8');
+const decryptWithRSA = function (hexCiphertext) {
+  var buffer = Buffer.from(hexCiphertext, 'hex');
+  var plaintext = crypto.privateDecrypt({
+    key: privateKey,
+    passphrase: passphrase,
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
+  }, buffer);
+  return plaintext;
+};
+
+const sha256 = function (data) {
+  const hash = crypto.createHash('sha256');
+  hash.update(data);
+  return hash.digest();
 }
 
-const publicKey = getPublicKey();
-const privateKey = getPrivateKey();
-var buffer =  Buffer.from('what');
-var ciphertext = crypto.publicEncrypt(publicKey, buffer);
-console.log(ciphertext);
-var plaintext = crypto.privateDecrypt({
-  key: privateKey,
-  passphrase: 'AKSenejfkwnEjDniw',
-  padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
-}, ciphertext);
-console.log(plaintext.toString());
+module.exports = {
+  // RSA
+  getPublicKey,
+  decryptWithRSA,
 
+  // Diffie Hellman
+  generateSessionKeys,
 
-// var encryptStringWithRsaPublicKey = function(toEncrypt) {
-//   var publicKey = getPublicKey();
-//   var buffer = new Buffer(toEncrypt);
-//   var encrypted = crypto.publicEncrypt(publicKey, buffer);
-//   return encrypted.toString("base64");
-// };
+  // hash
+  sha256,
 
-// var decryptStringWithRsaPrivateKey = function(toDecrypt) {
-//   var privateKey = getPrivateKey();
-//   var buffer = new Buffer(toDecrypt, "base64");
-//   var decrypted = crypto.privateDecrypt(privateKey, buffer);
-//   return decrypted.toString("utf8");
-// };
-
-// var msg = 'i like to shit';
-// var ciphertext = encryptStringWithRsaPublicKey(msg);
-// var plaintext = decryptStringWithRsaPrivateKey(msg);
+  // AES-256-CBC
+  encryptWithAES256CBC,
+  decryptWithAES256CBC,
+}
